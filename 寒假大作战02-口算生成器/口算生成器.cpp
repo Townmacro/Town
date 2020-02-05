c592e6387bdd40bae758e79212247b311cd830f7
冷漠时光彡丿 <1146934258@qq.com>
2020-02-05 13:00:31 +0800

    new code

--- 寒假大作战02-口算生成器/口算生成器.cpp
+++ 寒假大作战02-口算生成器/口算生成器.cpp
@@ -1,18 +1,68 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 void First_grade_problem()
 {
-	printf("现在是一年级题目：\n执行完了（学习使你快乐\n\n");
+	int n;
+	char symbol[] = { '+','-' };
+	scanf("%d", &n);
+	for (size_t i = 0; i < n; i++)
+	{
+		printf("%d %c %d=____\n", rand() % 10, symbol[rand() % 2], rand() % 10);
+	}
 }
 
 void Second_grade_problem()
 {
-	printf("现在是二年级题目：\n执行完了（学习使你快乐\n\n");
+	int n;
+	char symbol[] = { '*','/' };
+	scanf("%d", &n);
+	for (size_t i = 0; i < n; i++)
+	{
+		if (rand() % 2 == 1)
+		{
+			printf("%2d %c %2d=____\n", rand() % 100, symbol[1], 1 + rand() % 99);
+		}
+		else
+		{
+			printf("%2d %c %2d=____\n", rand() % 100, symbol[0], rand() % 100);
+		}
+	}
 }
 
 void Third_grade_problem()
 {
-	printf("现在是三年级题目：\n执行完了（学习使你快乐？\n\n");
+	int n, symbol1, symbol2;
+	char symbol[] = { '+','-','*','/' };
+	scanf("%d", &n);
+	for (size_t i = 0; i < n; i++)
+	{
+		if (rand() % 4 == 3)
+		{
+			symbol1 = rand() % 4;
+			symbol2 = rand() % 4;
+			if (symbol1 == 3)
+			{
+				printf("%2d %c %2d ", rand() % 100, symbol[3], 1 + rand() % 99);
+			}
+			else
+			{
+				printf("%2d %c %2d ", rand() % 100, symbol[rand() % 3], rand() % 100);
+			}
+			if (symbol2 == 3)
+			{
+				printf("%c %2d=____\n", symbol[3], 1 + rand() % 99);
+			}
+			else
+			{
+				printf("%c %2d=____\n", symbol[rand() % 3], rand() % 100);
+			}
+		}
+		else
+		{
+			printf("%2d %c %2d %c %2d=____\n", rand() % 100, symbol[rand() % 3], rand() % 100, symbol[rand() % 3], rand() % 100);
+		}
+	}
 }
 
 void menu()
@@ -40,14 +90,17 @@ int main()
 		switch (choose)
 		{
 		case 1:
+			printf("\n\n现在是一年级题目：\n请输入生成个数>");
 			First_grade_problem();
 			menu();
 			break;
 		case 2:
+			printf("\n\n现在是二年级题目：\n请输入生成个数>");
 			Second_grade_problem();
 			menu();
 			break;
 		case 3:
+			printf("\n\n现在是三年级题目：\n请输入生成个数>");
 			Third_grade_problem();
 			menu();
 			break;
